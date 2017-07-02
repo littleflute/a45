5e709422ff3f027c2eb535e60b05efd4b7dd61e4
littleflute <13699175164@sina.cn>
2017-07-02 15:31:33 -0500

    Add files via upload

---
+++ B_01-08/cd5/m.js
@@ -0,0 +1,15 @@
+var f = [];				
+f[0]	= "v0.0.9";			
+f[1]	= "Mary Pope Osborne   Interview with the Author.mp3";		
+f[2]	= "Midnight on the Moon (Book _8)   Earth Life.mp3";		
+f[3]	= "Midnight on the Moon (Book _8)   Hang On.mp3";		
+f[4]	= "Midnight on the Moon (Book _8)   High Jump.mp3";		
+f[5]	= "Midnight on the Moon (Book _8)   Moon Rabbits.mp3";		
+f[6]	= "Midnight on the Moon (Book _8)   Morgan.mp3";		
+f[7]	= "Midnight on the Moon (Book _8)   One Star to Another.mp3";		
+f[8]	= "Midnight on the Moon (Book _8)   Open Sesame.mp3";		
+f[9]	= "Midnight on the Moon (Book _8)   Prologue, By Moonlight.mp3";		
+f[10]	= "Midnight on the Moon (Book _8)   Space Motel.mp3";		
+f[11]	= "Midnight on the Moon (Book _8)   The Moon Man.mp3";		
+f[12]	= "Sunset of the Sabertooth (Book _7)   Master of the Animals.mp3";		
+f[13]	= "Sunset of the Sabertooth (Book _7)   This Age.mp3";		
